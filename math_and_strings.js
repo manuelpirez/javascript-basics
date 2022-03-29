6f891da8ce8236d00fbb2753f89267e1b75f84a6
manuelpirez <manuelpirez@gmail.com>
2022-03-28 22:58:39 -0600

    Create math_and_strings.js

---
+++ math_and_strings.js
@@ -0,0 +1,13 @@
+console.log("4" * 3) 	// 12
+
+console.log("-4" * 3) 	// -12
+
+console.log("4" / 3) 	// 1.3333333333333333
+
+console.log("4" % 3) 	// 1 - integer residue of a division
+
+console.log("4" - 3) 	// 1
+
+console.log("4.5" * 3) 	// 13.5
+
+console.log("4" + 3) 	// 43
